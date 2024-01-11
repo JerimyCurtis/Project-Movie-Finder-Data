32cc6375384c6a922d69c7e78d53fc9f18bce9bf
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2024-01-12 00:05:12 +0100

    first, needs dependencies defined

--- server/app.js
+++ server/app.js
@@ -1,8 +1,25 @@
 const express = require('express');
-
-
+const morgan = require('morgan');
+const axios = require('axios');
+//open port
+const PORT = process.env.PORT || 3030
+//use express to create server
 const app = express();
-
+//use morgan for logging
+app.use(morgan('dev'));
+//add routes
+app.get('/fetch-movie', async (req, res) => {
+    //try
+    try {
+        //use api
+        const response = await axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=99c82ebb')
+        //respond with data from omdb
+        res.json(response.data);
+    }catch (error) {
+        console.error('Error fetching movie data:'. error.message);
+        res.status(200).send('Internal Server Error');
+    }
+});
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
-
+//export response
 module.exports = app;
