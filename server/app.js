b1da98872e78f74c033777628852865e6d5ddb21
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2024-01-12 01:12:07 +0100

    needs routing

--- server/app.js
+++ server/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
-const axios = require('axios');
+const axios = require('axios-mock-adapter');
+require('dotenv').config();
+//add body-parser to json and .js
+var bodyParser = require('body-parser');
 //open port
 const PORT = process.env.PORT || 3030
 //use express to create server
 const app = express();
 //use morgan for logging
 app.use(morgan('dev'));
+//parse app
+app.use(bodyParser.urlencoded({extended: false}));
+//use parser
+app.use(bodyParser.json());
 //add routes
-app.get('/fetch-movie', async (req, res) => {
+app.get('/', function (req, res) => {
     //try
     try {
         //use api
