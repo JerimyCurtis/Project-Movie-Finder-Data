6d3049756649e64a53ed477d1adcc12db9442ab9
oxbojyd <153137143+oxbojyd@users.noreply.github.com>
2024-01-23 20:17:31 +0100

    removed old code

--- server/app.js
+++ server/app.js
@@ -1,40 +1,3 @@
-// const express = require('express');
-// const morgan = require('morgan');
-// const axios = require('axios');
-// require('dotenv').config(); // Load dotenv configuration first
-// const omdbApiKey = process.env.OMDB_API_KEY;
-// //add body-parser to json and .js
-// const bodyParser = require('body-parser');
-// //open port
-// const PORT = process.env.PORT || 3030
-// //use express to create server
-// const app = express();
-// //use morgan for logging
-// app.use(morgan('dev'));
-// //parse app
-// app.use(bodyParser.urlencoded({ extended: false }));
-// //use parser
-// app.use(bodyParser.json());
-// // Define a route for the root path
-// app.get('/', (req, res) => {
-//     res.send('Hello, this is your Express server!');
-//   });
-// //add routes
-// app.get('/fetch-movie', async (req, res) => {
-//     //try
-//     try {
-//         //use api
-//         const response = await axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=${omdbApiKey}`);
-//         //respond with data from omdb
-//         res.json(response.data);
-//     } catch (error) {
-//         console.error('Error fetching movie data:', error.message);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
-// //export response
-// module.exports = app;
-
 const express = require('express');
 const morgan = require('morgan');
 const axios = require('axios');
@@ -46,7 +9,7 @@ const omdbApiKey = process.env.OMDB_API_KEY;
 const bodyParser = require('body-parser');
 
 // Open port
-// const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3030;
 
 // Use express to create a server
 const app = express();
@@ -58,7 +21,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 // Parse application/json
 app.use(bodyParser.json());
-console.log("line 61",process.env.OMDB_API_KEY)
+console.log("line 24",process.env.OMDB_API_KEY)
 
 // Define a route for the root path
 app.get('/', (req, res) => {
@@ -69,7 +32,7 @@ app.get('/', (req, res) => {
 app.get('/fetch-movie', async (req, res) => {
     try {
         // Use Axios to make a request to OMDb API
-        const response = await axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=omdbApiKey`);
+        const response = await axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=${omdbApiKey}`);
         // Respond with the data from OMDb
         res.json(response.data);
     } catch (error) {
